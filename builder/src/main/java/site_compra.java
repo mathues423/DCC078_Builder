ced4a09c3c243acde54b0e4f0c55c722063bfd4a
Matheus dos Reis Casarim <37851549+mathues423@users.noreply.github.com>
2022-10-04 22:23:46 -0300

    Add files via upload

---
+++ builder/src/main/java/site_compra.java
@@ -0,0 +1,110 @@
+public class site_compra {
+    public class site_compra {
+        //Dados
+        String name;
+        String mid_name;
+        String last_name;
+        int cpf;
+        String bairo;
+        String rua;
+        String complemento;
+        int numero;
+        int cep;
+
+        //Contato
+        String email;
+        int cel;
+
+        public site_compra(){
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMid_name() {
+            return mid_name;
+        }
+
+        public void setMid_name(String mid_name) {
+            this.mid_name = mid_name;
+        }
+
+        public String getLast_name() {
+            return last_name;
+        }
+
+        public void setLast_name(String last_name) {
+            this.last_name = last_name;
+        }
+
+        public int getCpf() {
+            return cpf;
+        }
+
+        public void setCpf(int cpf) {
+            this.cpf = cpf;
+        }
+
+        public String getBairo() {
+            return bairo;
+        }
+
+        public void setBairo(String bairo) {
+            this.bairo = bairo;
+        }
+
+        public String getRua() {
+            return rua;
+        }
+
+        public void setRua(String rua) {
+            this.rua = rua;
+        }
+
+        public String getComplemento() {
+            return complemento;
+        }
+
+        public void setComplemento(String complemento) {
+            this.complemento = complemento;
+        }
+
+        public int getNumero() {
+            return numero;
+        }
+
+        public void setNumero(int numero) {
+            this.numero = numero;
+        }
+
+        public int getCep() {
+            return cep;
+        }
+
+        public void setCep(int cep) {
+            this.cep = cep;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getCel() {
+            return cel;
+        }
+
+        public void setCel(int cel) {
+            this.cel = cel;
+        }
+    }
+}
